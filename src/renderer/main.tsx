31bf57d572c89ba91c218e894a6af0e949220da5
夭夭 <zhangyuanyuan828@outlook.com>
2022-05-03 20:04:24 +0800

    chore: add React Suite

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
-import './index.css'
+import 'rsuite/dist/rsuite.css'
+import { App } from './App'
 
 createRoot(document.getElementById('root') || document.body.appendChild(document.createElement('div'))).render(
   <React.StrictMode>
